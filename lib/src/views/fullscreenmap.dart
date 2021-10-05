e98a01530382d3852c39bef6ca8777cfa30754cd
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-04 22:54:03 -0500

    Mapbox Studio - Change the style of our map

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -9,7 +9,12 @@ class FullScreenMap extends StatefulWidget {
 }
 
 class _FullScreenMapState extends State<FullScreenMap> {
+    final center = const LatLng(21.233008061802607, -98.48597525328837);
+    String selectedStyle = "mapbox://styles/alons01/ckudj4om602av17po7cxymqp6";
+    final darkStyle   = "mapbox://styles/alons01/ckudiz8oc022e17rhov55jywt";
+    final streetStyle = "mapbox://styles/alons01/ckudj4om602av17po7cxymqp6";
     MapboxMapController? mapController;
+
     void _onMapCreated(MapboxMapController controller) {
       mapController = controller;
     }
@@ -17,15 +22,33 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MapboxMap(
-        accessToken:"pk.eyJ1IjoiYWxvbnMwMSIsImEiOiJja3VkYnM4Y3ExOHF1MnBueml6anE5NDl3In0.hLC6trsF0h2NVj1c9lzVEQ",
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(21.233008061802607, -98.48597525328837),
-          zoom: 14
+        body: _createMap(),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              child: const Icon(Icons.add_to_home_screen),
+              onPressed: (){
+                setState(() {
+                  selectedStyle = (selectedStyle == darkStyle) ? streetStyle : darkStyle;
+                });
+              }
+            ),
+          ],
         ),
-        onStyleLoadedCallback: onStyleLoadedCallback,
-      )
+    );
+  }
+
+  MapboxMap _createMap() {
+    return MapboxMap(
+      styleString: selectedStyle,
+      accessToken:"pk.eyJ1IjoiYWxvbnMwMSIsImEiOiJja3VkYnM4Y3ExOHF1MnBueml6anE5NDl3In0.hLC6trsF0h2NVj1c9lzVEQ",
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: center,
+        zoom: 14
+      ),
+      onStyleLoadedCallback: onStyleLoadedCallback,
     );
   }
 }
