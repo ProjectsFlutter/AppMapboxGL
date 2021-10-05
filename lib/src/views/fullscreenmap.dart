3b4a6146304ee36ab9c4cc558d81bef82c4786a0
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-04 22:04:43 -0500

    Show map using MapBoxGL

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-class FullScreenMap extends StatelessWidget {
+class FullScreenMap extends StatefulWidget {
   const FullScreenMap({Key? key}) : super(key: key);
 
   @override
+  State<FullScreenMap> createState() => _FullScreenMapState();
+}
+
+class _FullScreenMapState extends State<FullScreenMap> {
+    MapboxMapController? mapController;
+    void _onMapCreated(MapboxMapController controller) {
+      mapController = controller;
+    }
+    void onStyleLoadedCallback() {}
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World'),
-      ),
+    return Scaffold(
+        body: MapboxMap(
+        accessToken:"pk.eyJ1IjoiYWxvbnMwMSIsImEiOiJja3VkYnM4Y3ExOHF1MnBueml6anE5NDl3In0.hLC6trsF0h2NVj1c9lzVEQ",
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(21.233008061802607, -98.48597525328837),
+          zoom: 14
+        ),
+        onStyleLoadedCallback: onStyleLoadedCallback,
+      )
     );
   }
 }
